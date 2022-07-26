9363a1bb65d26140ced4307dc78686821a36dbc0
Yevhen Khreptun <khreptun.e@gmail.com>
2022-07-26 22:00:37 +0300

    addad apex chart and projects block

---
+++ js/main.js
@@ -0,0 +1,22 @@
+let transactionChartOptions = {
+  series: [55, 25, 20],
+  chart: {
+    type: 'donut',
+  },
+  colors:['#000', '#00d25b', '#ffab00'],
+  labels: ['Paypal', 'Cash', 'Stripe'],
+  legend: {
+    show: false
+  },
+  dataLabels: {
+    enabled: false
+  },
+  plotOptions: {
+    pie: {
+      size: 200
+    }
+  }
+};
+
+let transactionChart = new ApexCharts(document.querySelector("#diagram"), transactionChartOptions);
+transactionChart.render();
